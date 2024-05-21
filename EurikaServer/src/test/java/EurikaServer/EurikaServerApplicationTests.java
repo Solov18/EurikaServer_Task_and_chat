dfa4f28a30963743c94acf657c460e877f1cf951
Solovev Evgenij <123502849+Solov18@users.noreply.github.com>
2024-05-21 21:13:46 +0300

    Add files via upload

---
+++ EurikaServer/src/test/java/EurikaServer/EurikaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package EurikaServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurikaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
